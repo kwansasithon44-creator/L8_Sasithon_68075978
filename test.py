f6aa15b46aec0d388d1b38aa327f963aea83b657
kwansasithon44-creator <kwansasithon44@gmail.com>
2026-03-03 01:12:10 +0700

    Add files via upload

---
+++ test.py
@@ -0,0 +1,13 @@
+print("Hello world")
+
+# ข้อ 1
+age = 25
+print("อายุของฉันคือ", age)
+
+# ข้อ 2
+name = "ชื่อนักศึกษา"
+age = 22
+print(f"สวัสดี {name}, อายุ {age} ปี")
+
+# ข้อ 3
+print("Python", "Java", "C++", sep=" | ")
